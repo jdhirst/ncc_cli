2edfffe530eaf47d6ed8996d14b482b3dd79ddc2
Nils Achtergarde <achtergarde@gbv.de>
2020-01-06 14:50:11 +0100

    build/lib/ncc_cli/ncc_cli.py: Updated the file ncc_cli.py in build directory, too

--- build/lib/ncc_cli/ncc_cli.py
+++ build/lib/ncc_cli/ncc_cli.py
@@ -3,7 +3,7 @@ import sys
 import os
 import subprocess
 import getpass
-import ConfigParser
+import configparser
 import base64
 import time
 import humanize
@@ -25,7 +25,7 @@ if os.name == 'nt':
     configfile = home + "/ncc_cfg.ini"
 else:
     configfile = home + "/.ncc_cfg"
-Config = ConfigParser.ConfigParser()
+Config = configparser.ConfigParser()
 Config.read(configfile)
 cwd = os.getcwd()
 
@@ -45,7 +45,7 @@ def ConfigSectionMap(section):
 def connect():
     global session
     if not os.path.isfile(configfile):
-        print bcolors.YELLOW + "--- Initial Client Setup ---\n\nNOTE: Please enter full URI for server name.\nExample: https://myserver.com/nextcloud\nFor security reasons, please do not use your plaintext password for this utility. You should generate an App password from your NextCloud settings page.\n" + bcolors.DEFAULT
+        print (bcolors.YELLOW + "--- Initial Client Setup ---\n\nNOTE: Please enter full URI for server name.\nExample: https://myserver.com/nextcloud\nFor security reasons, please do not use your plaintext password for this utility. You should generate an App password from your NextCloud settings page.\n" + bcolors.DEFAULT)
         servername_input = raw_input('Server URI: ')
         username_input = raw_input('Username: ')
         password_raw = getpass.getpass()
@@ -60,7 +60,7 @@ def connect():
     cfguser = ConfigSectionMap("Config")['username']
     cfgpass_encoded = ConfigSectionMap("Config")['password_base64']
     servername = ConfigSectionMap("Config")['servername']
-    cfgpass = base64.decodestring(cfgpass_encoded)
+    cfgpass = base64.b64decode(cfgpass_encoded)
     session = nclib.Client(servername)
     session.login(cfguser,cfgpass)
 
@@ -87,9 +87,9 @@ def dl_progress():
 
 def zip_dl_progress():
     if os.path.exists(dl_lpath):
-        print dl_lpath + " already exists"
+        print (dl_lpath + " already exists")
     else:
-        print "Downloading zip of " + getdirarg1
+        print ("Downloading zip of " + getdirarg1)
         dl_lsize = 1
         getdir_copy = multiprocessing.Process(name='getdir_copy', target=getdirproc)
         getdir_copy.start()
@@ -105,7 +105,7 @@ def zip_dl_progress():
             changesize = float(dl_lsize) - float(oldsize)
             pbar.update(changesize)
         pbar.close()
-        print "Download Complete"
+        print ("Download Complete")
 
 ##### DEFINE USER COMMANDS BELOW #####
 
@@ -128,7 +128,7 @@ def ls(arg1,arg2=False):
                 if i.is_dir() == False:
                     t.add_row(["",bcolors.DEFAULT + i.get_name() + bcolors.DEFAULT,humanize.naturalsize(i.get_size())])
         t.set_deco(t.VLINES)
-        print t.draw()
+        print (t.draw())
     #except:
     #    print "Error: could not list dir"
 
@@ -141,46 +141,46 @@ def lsshare(arg1):
         for i in output:
             t.add_row([i.get_path(),i.get_token(),i.get_share_time()])
         t.set_deco(t.VLINES | t.HEADER)
-        print t.draw()
+        print (t.draw())
     except:
-        print "Error: could not list shares"
+        print ("Error: could not list shares")
 
 def put(arg1,arg2='/'):
     try:
-        print "Uploading " + arg1
+        print ("Uploading " + arg1)
         with yaspin(Spinners.bouncingBall, attrs=["bold"],) as sp:
             sp.text = "Operation in Progress"
             connect()
             session.put_file(arg2,arg1)
             time.sleep(1.0)
-        print "Upload Complete"
+        print ("Upload Complete")
 
     except:
-        print "Error: could not upload file"
+        print ("Error: could not upload file")
     # TODO: Better progress indication. Need library update for this.
 
 def putdir(arg1,arg2='/'):
     try:
-        print "Uploading directory " + arg1
+        print ("Uploading directory " + arg1)
         with yaspin(Spinners.bouncingBall, attrs=["bold"],) as sp:
             sp.text = "Operation in Progress"
             connect()
             session.put_directory(arg2,arg1)
             time.sleep(1.0)
-        print "Upload Complete"
+        print ("Upload Complete")
 
     except:
-        print "Error: could not upload directory"
+        print ("Error: could not upload directory")
     # TODO: Better progress indication. Need library update for this.
 
 def getproc():
     try:
-        print "Downloading " + getarg1
+        print ("Downloading " + getarg1)
         session.get_file(getarg1,getarg2)
         time.sleep(1.0)
-        print "Download Complete"
+        print ("Download Complete")
     except:
-        print "Error: could not download file"
+        print ("Error: could not download file")
 
 def get(arg1,arg2=None):
     global getarg1
@@ -198,7 +198,7 @@ def getdirproc():
     try:
         session.get_directory_as_zip(getdirarg1,getdirarg2)
     except:
-        print "Error: could not download zip file"
+        print ("Error: could not download zip file")
 
 def getdir(arg1,arg2=None):
     global getdirarg1
@@ -216,31 +216,31 @@ def mkdir(arg1):
             connect()
             session.mkdir(arg1)
             time.sleep(1.0)
-        print "Created " + arg1
+        print ("Created " + arg1)
     except: 
-        print "Error: could not create dir"
+        print ("Error: could not create dir")
 
 def copy(arg1,arg2):
-    print "Copying " + arg1 + " to " + arg2
+    print ("Copying " + arg1 + " to " + arg2)
     with yaspin(Spinners.bouncingBall, attrs=["bold"],) as sp:
         sp.text = "Operation in Progress"
         connect()
         session.copy(arg1,arg2)
         time.sleep(1.0)
-    print "Operation Completed"
+    print ("Operation Completed")
 
 def move(arg1,arg2):
-    print "Moving " + arg1 + " to " + arg2
+    print ("Moving " + arg1 + " to " + arg2)
     with yaspin(Spinners.bouncingBall, attrs=["bold"],) as sp:
         sp.text = "Operation in Progress"
         connect()
         session.move(arg1,arg2)
         time.sleep(1.0)
-    print "Operation Completed"
+    print ("Operation Completed")
 
 def rm(arg1):
     try:
-        print "WARNING: This command deletes files recursively."
+        print ("WARNING: This command deletes files recursively.")
         confirm = ""
         while confirm != "Y" and confirm != "N" and confirm != "y" and confirm != "n":
             confirm = raw_input('Are you sure you want to delete ' + arg1 + '? [Y/N]')
@@ -250,29 +250,29 @@ def rm(arg1):
                 connect()
                 session.delete(arg1)
                 time.sleep(1.0)
-            print "Deleted " + arg1   
+            print ("Deleted " + arg1)   
         elif confirm == "N" or confirm == "n":
-            print "Operation Canceled"
+            print ("Operation Canceled")
             sys.exit(0)
     except: 
-        print "Error: could not delete"
+        print ("Error: could not delete")
 
 def mkshare(arg1):
     try:
-        print "Creating public share"
+        print ("Creating public share")
         with yaspin(Spinners.bouncingBall, attrs=["bold"],) as sp:
             sp.text = "Operation in Progress"
             connect()
             share = session.share_file_with_link(arg1)
             time.sleep(1.0)
-        print "\n=== Created Share Successfully ==="+ bcolors.YELLOW + "\n\nLink: " + share.get_link() + "\nShare Contents: " + share.get_path() + "\n" + bcolors.DEFAULT
+        print ("\n=== Created Share Successfully ==="+ bcolors.YELLOW + "\n\nLink: " + share.get_link() + "\nShare Contents: " + share.get_path() + "\n" + bcolors.DEFAULT)
 
     except:
-        print "Error: could not create file share"
+        print ("Error: could not create file share")
 
 def rmshare(arg1):
     try:
-        print "Loading Share: " + arg1
+        print ("Loading Share: " + arg1)
         confirm = ""
         with yaspin(Spinners.bouncingBall, attrs=["bold"],) as sp:
             sp.text = "Operation in Progress"
@@ -282,21 +282,21 @@ def rmshare(arg1):
             for i in output:
                 sharepath = i.get_path()
         if sharepath == "":
-            print "Share not found"
+            print ("Share not found")
         else:
             while confirm != "Y" and confirm != "N" and confirm != "y" and confirm != "n":
                 confirm = raw_input('Are you sure you want to delete share ' + sharepath + '? [Y/N]')
             if confirm == "Y" or confirm == "y":
-                print "Deleting public share"
+                print ("Deleting public share")
                 with yaspin(Spinners.bouncingBall, attrs=["bold"],) as sp:
                     for i in output:
                         sp.text = "Operation in Progress"
                         session.delete_share(i.get_id())
                         time.sleep(1.0)
-            print "\nOperation Completed"
+            print ("\nOperation Completed")
 
     except:
-        print "Error: could not delete file share"
+        print ("Error: could not delete file share")
 ##### END COMMAND DEFINITIONS #####
 
 def main():
@@ -305,8 +305,8 @@ def main():
     if (len(sys.argv) == 1) or (sys.argv[1] == "-h") or (sys.argv[1] == "--help"):
         t = Texttable()
         t.set_deco(t.VLINES)
-        print "No arguments provided."
-        print "\nSyntax: " + sys.argv[0] + " [command] <arguments>\n"
+        print ("No arguments provided.")
+        print ("\nSyntax: " + sys.argv[0] + " [command] <arguments>\n")
         t.add_row(["ls","(-l) <dir>","lists dir contents"])
         t.add_row(["put","[local file] <dest remote dir>","uploads file"])
         t.add_row(["putdir","[local dir] <dest remote dir>","uploads dir"])
@@ -319,7 +319,7 @@ def main():
         t.add_row(["rmshare","[dir/file name]","deletes share"])
         t.add_row(["cp","[source] [destination]","copies files on server"])
         t.add_row(["mv","[source] [destination]","moves files on server"])
-        print t.draw()
+        print (t.draw())
         sys.exit()
     elif len(sys.argv) >= 2:
         if sys.argv[1] == "ls":
@@ -347,19 +347,19 @@ def main():
 
         elif sys.argv[1] == "mkdir":
             if len(sys.argv) == 2:
-                print "Creates a new directory\nSyntax: mkdir <name>"
+                print ("Creates a new directory\nSyntax: mkdir <name>")
             else: 
                 mkdir(sys.argv[2])
 
         elif sys.argv[1] == "rm":
             if len(sys.argv) == 2:
-                print "Recursively deletes a file or directory\nSyntax: rm <name>"
+                print ("Recursively deletes a file or directory\nSyntax: rm <name>")
             else: 
                 rm(sys.argv[2])
 
         elif sys.argv[1] == "put":
             if len(sys.argv) == 2:
-                print "Uploads a file\nSyntax: put [local source] <remote dest>"
+                print ("Uploads a file\nSyntax: put [local source] <remote dest>")
             elif len(sys.argv) == 3:
                 put(sys.argv[2])
             elif len(sys.argv) == 4:
@@ -367,7 +367,7 @@ def main():
 
         elif sys.argv[1] == "putdir":
             if len(sys.argv) == 2:
-                print "Uploads a directory\nSyntax: putdir [local source] <remote dest>"
+                print ("Uploads a directory\nSyntax: putdir [local source] <remote dest>")
             elif len(sys.argv) == 3:
                 putdir(sys.argv[2])
             elif len(sys.argv) == 4:
@@ -375,7 +375,7 @@ def main():
 
         elif sys.argv[1] == "get":
             if len(sys.argv) == 2:
-                print "Downloads a file\nSyntax: get [remote source] <local dest>"
+                print ("Downloads a file\nSyntax: get [remote source] <local dest>")
             elif len(sys.argv) == 3:
                 dl_lpath = "./" + sys.argv[2].rsplit('/', 1)[-1]
                 dl_rpath = sys.argv[2]
@@ -386,7 +386,7 @@ def main():
                 get(sys.argv[2],sys.argv[3] + '/' + sys.argv[2].rsplit('/', 1)[-1])
         elif sys.argv[1] == "getdir":
             if len(sys.argv) == 2:
-                print "Downloads directory as zip archive\nSyntax: getdir [remote source] <local dest>"
+                print ("Downloads directory as zip archive\nSyntax: getdir [remote source] <local dest>")
             elif len(sys.argv) == 3:
                 dl_lpath = "./" + sys.argv[2].rsplit('/', 1)[-1] + '.zip'
                 dl_rpath = sys.argv[2]
@@ -397,32 +397,32 @@ def main():
                 getdir(sys.argv[2],sys.argv[3])
         elif sys.argv[1] == "cp":
             if (len(sys.argv) == 2) or (len(sys.argv) == 3):
-                print "Copies file within cloud\nSyntax: cp [source file] [destination dir]"
+                print ("Copies file within cloud\nSyntax: cp [source file] [destination dir]")
             elif len(sys.argv) == 4:
                 copy(sys.argv[2],sys.argv[3] + '/' + sys.argv[2].rsplit('/', 1)[-1])
         elif sys.argv[1] == "mv":
             if (len(sys.argv) == 2) or (len(sys.argv) == 3):
-                print "Moves file within cloud\nSyntax: mv [source file] [destination dir]"
+                print ("Moves file within cloud\nSyntax: mv [source file] [destination dir]")
             elif len(sys.argv) == 4:
                 move(sys.argv[2],sys.argv[3] + '/' + sys.argv[2].rsplit('/', 1)[-1])
         elif sys.argv[1] == "mkshare":
             if len(sys.argv) == 2:
-                print "Creates new share with link from supplied file/directory\nSyntax: mkshare [remote file/dirname]"
+                print ("Creates new share with link from supplied file/directory\nSyntax: mkshare [remote file/dirname]")
             else: 
                 mkshare(sys.argv[2])
         elif sys.argv[1] == "rmshare":
             if len(sys.argv) == 2:
-                print "Deletes file share from supplied file/directory\nSyntax: rmshare [remote file/dirname]"
+                print ("Deletes file share from supplied file/directory\nSyntax: rmshare [remote file/dirname]")
             else: 
                 rmshare(sys.argv[2])
         else:
-            print "Invalid Command, please revise."
+            print ("Invalid Command, please revise.")
     else:
-        print "Invalid Syntax, please revise."
+        print ("Invalid Syntax, please revise.")
 
 if __name__ == '__main__':
     try: 
         main()
     except KeyboardInterrupt:
-        print "Operation Interrupted"
+        print ("Operation Interrupted")
         sys.exit()
