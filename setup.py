217d38b9dd7f6a04b217d16756dea56bdf3ce391
James Hirst <jhirst@sandvine.com>
2020-07-08 13:03:11 -0400

    Updated to 0.4; cleaned up repository.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ncc_cli",
-    version="0.0.3",
+    version="0.0.4",
     author="James Hirst",
     author_email="jdhirst@hirstgroup.net",
     description="A small command-line client for NextCloud/ownCloud",
